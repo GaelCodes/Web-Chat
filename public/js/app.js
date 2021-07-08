3ad81efce165cb1051b53ff332f3103729ba4a52
Gael <abigael-hf@hotmail.com>
2021-07-08 02:24:35 +0200

    Funcionalidad de autenticación con Firebase Authentication añadida

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,66 @@
 var db = firebase.firestore();
+var auth = firebase.auth;
 
-var docRef = db.collection("usuarios").doc("abigael-hf@hotmail.com");
-var newMessageRef = db.doc("usuarios/abigael/conversaciones/abigael2/mensajes/mensaje3");
+var usersCollection = db.collection("usuarios");
 
-// newMessageRef.add()
-// docRef.set()
-// docRef.get()
+
+/* 
+    FIREBASE AUTHENTICATION
+*/
+
+// SIGN UP
+function createUser(email, password) {
+    auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredentials) => {
+            var userData = userCredentials.user;
+
+            var newUserDoc = usersCollection.doc(`${userData.uid}`);
+
+            newUserDoc.set({
+                    nombre: "",
+                    email: userData.email,
+                    photoURL: "",
+                })
+                .then(() => {
+                    // Navegar a HOME
+                    console.log('Navegando a Home...');
+
+
+
+                }).catch((error) => {
+                    console.log('Se ha producido el siguiente error: ', error);
+                });
+        })
+        .catch((error) => {
+
+            console.log(`Se ha producido el siguiente error: ${error.code} :  ${error.message}`);
+
+        });
+}
+
+
+
+// LOGIN
+function loginUser(email, password) {
+    auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Navegar a HOME
+
+            var userData = userCredential.user;
+        })
+        .catch((error) => {
+            console.log(`Se ha producido el siguiente error: ${error.code} :  ${error.message}`);
+        });
+}
+
+
+// LOGOUT
+function logoutUser() {
+    auth().signOut()
+        .then(() => {
+            console.log('Signed Out');
+        })
+        .catch((error) => {
+            console.log(`Se ha producido el siguiente error: ${error}`);
+        });
+}
