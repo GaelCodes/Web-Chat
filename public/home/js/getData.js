e5657da6872f3cf68c993b3d7bee494b34475c65
Gael <abigael-hf@hotmail.com>
2021-12-10 11:42:51 +0100

    Fixes #10 - Evento a capturar modificado a keyup

--- public/home/js/getData.js
+++ public/home/js/getData.js
@@ -255,7 +255,7 @@ $(document).ready(function() {
     var sendButtonDesktop = '#sendButtonDesktop';
     $(sendButtonDesktop).click(sendMessage);
     $('#inputDesktop').on('input', enableButton);
-    $('#inputDesktop').on('keydown', (event) => {
+    $('#inputDesktop').on('keyup', (event) => {
         if (event.code === "Enter" && !$(sendButtonDesktop)[0].disabled) {
             $(sendButtonDesktop).click();
         }
